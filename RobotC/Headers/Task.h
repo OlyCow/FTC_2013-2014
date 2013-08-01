308dec5685ee32689e381da1f2f71f524c57311f
Ernest Gu <erniewarrensuper@gmail.com>
2013-08-01 15:47:02 -0700

    Added forward declarations for task functions.
    Yay :) Now for the implementation :|

--- RobotC/Headers/Task.h
+++ RobotC/Headers/Task.h
@@ -4,7 +4,22 @@
 
 
 
-// CODE!!! :D
+// Very unsure about this `void` thing.
+void Task_StartTask(void taskID, int priority=kDefaultTaskPriority);
+void Task_SuspendTask();
+void Task_SuspendTask(void taskID);
+void Task_StopTask();
+void Task_StopTask(void taskID);
+void Task_StopAllTasks();
+void Task_HogCPU();
+void Task_ReleaseCPU();
+void Task_EndTimeslice();
+void Task_SetPriority(int priority);
+void Task_SetPriority(void taskID, int priority);
+int  Task_GetPriority();
+int  Task_GetPriority(void taskID);
+void Task_SetTimesliceSize();
+int  Task_GetTimesliceSize();
 
 
 

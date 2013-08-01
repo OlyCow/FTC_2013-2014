384d028cf0194057fd35a313618f681c34a84be4
Ernest Gu <erniewarrensuper@gmail.com>
2013-08-01 16:09:43 -0700

    Preliminary implementation of Task.h
    What the heck is a `void`? A `void` function? Pointer?

--- RobotC/Headers/Task.h
+++ RobotC/Headers/Task.h
@@ -6,9 +6,9 @@
 
 // Very unsure about this `void` thing.
 void Task_StartTask(void taskID, int priority=kDefaultTaskPriority);
-void Task_SuspendTask();
+void Task_SuspendTask(); //If I can cast an `int` to a `void` this function can be combined with the next.
 void Task_SuspendTask(void taskID);
-void Task_StopTask();
+void Task_StopTask(); //If I can cast an `int` to a `void` this function can be combined with the next.
 void Task_StopTask(void taskID);
 void Task_StopAllTasks();
 void Task_HogCPU();
@@ -16,9 +16,9 @@ void Task_ReleaseCPU();
 void Task_EndTimeslice();
 void Task_SetPriority(int priority);
 void Task_SetPriority(void taskID, int priority);
-int  Task_GetPriority();
+int  Task_GetPriority(); //If I can cast an `int` to a `void` this function can be combined with the next.
 int  Task_GetPriority(void taskID);
-void Task_SetTimesliceSize();
+void Task_SetTimesliceSize(int size); //Source claims this is read-only...
 int  Task_GetTimesliceSize();
 
 
--- RobotC/Libraries/Task.c
+++ RobotC/Libraries/Task.c
@@ -6,8 +6,70 @@
 
 
 
-// CODE!!! :D
+// Very unsure about this `void` thing.
+void Task_StartTask(void taskID, int priority) {
+	StartTask(taskID, priority);
+}
+void Task_SuspendTask() {
+	suspendTask(nCurrentTask); //doing this may not be legal, since suspendTask expects a `void`
+}
+void Task_SuspendTask(void taskID) {
+	suspendTask(taskID);
+}
+void Task_StopTask() {
+	StopTask(nCurrentTask); //doing this may not be legal, since StopTask expects a `void`
+}
+void Task_StopTask(void taskID) {
+	StopTask(taskID);
+}
+void Task_StopAllTasks() {
+	StopAllTasks();
+}
+void Task_HogCPU() {
+	hogCPU();
+}
+void Task_ReleaseCPU() {
+	releaseCPU();
+}
+void Task_EndTimeslice() {
+	abortTimeslice();
+}
+void Task_SetPriority(int priority) {
+	setTaskPriority(nCurrentTask, priority); //doing this may not be legal, since setTaskPriority expects a `void`
+}
+void Task_SetPriority(void taskID, int priority) {
+	setTaskPriority(taskID, priority);
+}
+int  Task_GetPriority() {
+	return getTaskPriority(nCurrentTask); //doing this may not be legal, since getTaskPriority expects a `void`
+}
+int  Task_GetPriority(void taskID) {
+	return getTaskPriority(taskID);
+}
+void Task_SetTimesliceSize(int size) {
+	nOpcodesPerTimeslice = size; //may not be legal since nOpcodesPerTimeslice might be read-only :P
+}
+int  Task_GetTimesliceSize() {
+	return nOpcodesPerTimeslice;
+}
 
 
 
+
+//---abortTimeslice()
+//---EndTimeSlice()
+//---getTaskPriority(taskID)
+//---hogCPU()
+//---releaseCPU()
+//---StartTask(taskID)
+//---StartTask(taskID, priority)
+//---StopAllTasks()
+//---StopTask(taskID)
+//---suspendTask(taskID) //internal use only?
+//---bClearVariablesOnProgramStart //Not touching :P
+//---nOpcodesPerTimeslice //YEAH
+//---nSchedulePriority
+//---kDefaultTaskPriority (7)
+//---kLowPriority (0)
+//---kHighPriority (255)
 #endif // TASK_C

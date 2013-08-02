f67aa379ae8d203734a2eeb16d8311d297bd8aca
Ernest Gu <erniewarrensuper@gmail.com>
2013-08-02 12:19:52 -0700

    Added outline for Display.h

--- RobotC/Headers/Display.h
+++ RobotC/Headers/Display.h
@@ -8,5 +8,37 @@
 
 
 
+//eraseDisplay()
+//nxtSetPixel(xPos, yPos)
+//nxtClearPixel(xPos, yPos)
+//nxtDisplayBigStringAt(xPos, yPos, pChar, ...);
+//nxtDisplayBigStringAt(xPos, yPos, sFormatString, ...);
+//nxtDisplayBigTextLine(nLineNumber, pChar, ...);
+//nxtDisplayBigTextLine(nLineNumber, sFormatString, ...);
+//nxtDisplayCenteredBigTextLine(nLineNumber, pChar, ...);
+//nxtDisplayCenteredBigTextLine(nLineNumber, sFormatString, ...);
+//nxtDisplayCenteredTextLine(nLineNumber, pChar, ...);
+//nxtDisplayCenteredTextLine(nLineNumber, sFormatString, ...);
+//nxtDisplayRICFile(nleft, nBottom, sFileName);
+//nxtDisplayString(nLineNumber, pChar, ...);
+//nxtDisplayString(nLineNumber, sFormatString, ...);
+//nxtDisplayStringAt(nLineNumber, pChar, ...);
+//nxtDisplayStringAt(nLineNumber, sFormatString, ...);
+//nxtDisplayTextLine(nLineNumber, pChar, ...);
+//nxtDisplayTextLine(nLineNumber, sFormatString, ...);
+//nxtDrawEllipse(Left, Top, Right, Bottom);
+//nxtDrawLine(xPos, yPos, xPosTo, yPosTo);
+//nxtDrawRect(Left, Top, Right, Bottom);
+//nxtEraseEllipse(Left, Top, Right, Bottom);
+//nxtEraseLine(xPos, yPos, xPosTo, yPosTo);
+//nxtEraseRect(Left, Top, Right, Bottom);
+//nxtFillEllipse(Left, Top, Right, Bottom);
+//nxtFillRect(Left, Top, Right, Bottom);
+//nxtInvertLine(xPos, yPos, xPosTo, yPosTo);
+//nxtScrollText(pChar, ...);
+//nxtScrollText(sFormatString);
+//bNxtLCDStatusDisplay
+//nLCDContrast
+//nLCDContrastDefault
 #include "..\Libraries\Display.c"
 #endif // DISPLAY_H

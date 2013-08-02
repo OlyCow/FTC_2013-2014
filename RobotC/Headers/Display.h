fa8f7bbdc8d7ce3c4ad0c76f7a9a2a22db2f7fa2
Ernest Gu <erniewarrensuper@gmail.com>
2013-08-02 13:15:47 -0700

    Outlined display functions (for drawing)
    still need to do the text functions; I need to leave for Ian's though so
    I'll commit now.

--- RobotC/Headers/Display.h
+++ RobotC/Headers/Display.h
@@ -4,13 +4,20 @@
 
 
 
-// CODE!!! :D
+void Display_Clear(bool isNegative=false);
+void Display_DrawPixel(int x, int y, bool isNegative=false);
+void Display_DrawLine(int x1, int y1, int x2, int y2, bool isNegative=false);
+void Display_DrawLineXOR(int x1, int y1, int x2, int y2, bool isNegative=false);
+void Display_DrawRect(int L, int T, int R, int B, bool isNegative=false, bool isFilled=false);
+void Display_DrawCircle(int h, int k, int diameter, bool isNegative=false, bool isFilled=false);
+void Display_DrawEllipse(int L, int T, int R, int B, bool isNegative=false, bool isFilled=false);
+/* <---- (REMAINING) FUNCTIONS FOR TEXT GO HERE ----> */
 
 
 
-//eraseDisplay()
-//nxtSetPixel(xPos, yPos)
-//nxtClearPixel(xPos, yPos)
+//---eraseDisplay()
+//---nxtSetPixel(xPos, yPos)
+//---nxtClearPixel(xPos, yPos)
 //nxtDisplayBigStringAt(xPos, yPos, pChar, ...);
 //nxtDisplayBigStringAt(xPos, yPos, sFormatString, ...);
 //nxtDisplayBigTextLine(nLineNumber, pChar, ...);
@@ -26,15 +33,15 @@
 //nxtDisplayStringAt(nLineNumber, sFormatString, ...);
 //nxtDisplayTextLine(nLineNumber, pChar, ...);
 //nxtDisplayTextLine(nLineNumber, sFormatString, ...);
-//nxtDrawEllipse(Left, Top, Right, Bottom);
-//nxtDrawLine(xPos, yPos, xPosTo, yPosTo);
-//nxtDrawRect(Left, Top, Right, Bottom);
-//nxtEraseEllipse(Left, Top, Right, Bottom);
-//nxtEraseLine(xPos, yPos, xPosTo, yPosTo);
-//nxtEraseRect(Left, Top, Right, Bottom);
-//nxtFillEllipse(Left, Top, Right, Bottom);
-//nxtFillRect(Left, Top, Right, Bottom);
-//nxtInvertLine(xPos, yPos, xPosTo, yPosTo);
+//---nxtDrawEllipse(Left, Top, Right, Bottom);
+//---nxtDrawLine(xPos, yPos, xPosTo, yPosTo);
+//---nxtDrawRect(Left, Top, Right, Bottom);
+//---nxtEraseEllipse(Left, Top, Right, Bottom);
+//---nxtEraseLine(xPos, yPos, xPosTo, yPosTo);
+//---nxtEraseRect(Left, Top, Right, Bottom);
+//---nxtFillEllipse(Left, Top, Right, Bottom);
+//---nxtFillRect(Left, Top, Right, Bottom);
+//---nxtInvertLine(xPos, yPos, xPosTo, yPosTo);
 //nxtScrollText(pChar, ...);
 //nxtScrollText(sFormatString);
 //bNxtLCDStatusDisplay

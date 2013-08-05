141817db1034a98a79eba95021395888994f64e1
Ernest Gu <erniewarrensuper@gmail.com>
2013-08-05 13:29:22 -0700

    Fixed some issues with the Task library
    Now it works. Mhm.

--- RobotC/Headers/global vars.h
+++ RobotC/Headers/global vars.h
@@ -7,6 +7,7 @@
 
 // The number of internal timers available to use.
 const int g_TimerNumber = 4; //defined internally as macro `kNumbOfTimers`
+
 // The mode the IR sensor is set at.
 // The competition IR beam/emmiter pulses at 1200 hertz.
 const tHTIRS2DSPMode g_IRsensorMode = DSP_1200;
@@ -18,13 +19,18 @@ const int g_IRthreshold = 10; //units?
 // The highest we've ever recorded is +/-8.
 const int g_JoystickDeadZone = 10;
 
-// The factor to divide by when fine-tuning motors.
-const int g_FineTuneFactor = 4;
-
 // The various motor-regulation speeds.
 const int g_FullPower = 100;
-const int g_FullRegulatedPower = 80;
 
+// Each task will define a const variable with the same name as the task,
+// equal to the value of a call to `nCurrentTask`, to refer to an element
+// of this struct. `Task_Spawn()` and other "Task.h" functions will then
+// use `g_taskIndices[TASK_FOO_CONST]` to refer to that task.
+const int g_taskIndices[20] =
+	{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19}; // There can only be 20 tasks, including `main()`.
+	// Declared here instead of with a for loop because these should be const.
+
+// Various structs, initialized in `initializeGlobalVariables()`.
 motorData g_MotorData[4]; //4 drive base motors.
 joystickData g_JoystickData[2]; //2 controllers.
 joystickData g_PrevJoystickData[2]; //2 controllers.

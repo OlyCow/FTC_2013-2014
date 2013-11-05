8b48a1812e4101d07c15616b07742a761afbd786
Ernest Gu <erniewarrensuper@gmail.com>
2013-11-04 17:05:37 -0800

    What I worked on after school today~
    not much, just added a pickup dooliwhap

--- RobotC/Teleop-Omni.c
+++ RobotC/Teleop-Omni.c
@@ -1,9 +1,27 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  none,     none)
-#pragma config(Sensor, S3,     sensor_gyro,    sensorI2CCustomFastSkipStates9V)
+#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTMotor)
+#pragma config(Hubs,  S2, HTServo,  HTServo,  none,     none)
+#pragma config(Sensor, S3,     sensor_IR,      sensorI2CCustomFastSkipStates9V)
+#pragma config(Sensor, S4,     sensor_gyro,    sensorI2CCustomFastSkipStates9V)
 #pragma config(Motor,  mtr_S1_C1_1,     motor_FR,      tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C1_2,     motor_FL,      tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_1,     motor_BL,      tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     motor_FL,      tmotorTetrix, openLoop, reversed)
+#pragma config(Motor,  mtr_S1_C2_1,     motor_BL,      tmotorTetrix, openLoop, reversed)
 #pragma config(Motor,  mtr_S1_C2_2,     motor_BR,      tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     motor_sweeper, tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_2,     motor_lift,    tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_1,     motor_flag_L,  tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C4_2,     motor_flag_R,  tmotorTetrix, openLoop)
+#pragma config(Servo,  srvo_S2_C1_1,    servo1,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_2,    servo2,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_3,    servo3,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_4,    servo4,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_5,    servo5,               tServoNone)
+#pragma config(Servo,  srvo_S2_C1_6,    servo6,               tServoNone)
+#pragma config(Servo,  srvo_S2_C2_1,    servo7,               tServoNone)
+#pragma config(Servo,  srvo_S2_C2_2,    servo8,               tServoNone)
+#pragma config(Servo,  srvo_S2_C2_3,    servo9,               tServoNone)
+#pragma config(Servo,  srvo_S2_C2_4,    servo10,              tServoNone)
+#pragma config(Servo,  srvo_S2_C2_5,    servo11,              tServoNone)
+#pragma config(Servo,  srvo_S2_C2_6,    servo12,              tServoNone)
 
 #include "includes.h"
 
@@ -11,6 +29,7 @@
 
 task main()
 {
+	bool isSweeping = false; // For the stweet streeper motor. Controlled by BUTTON_A. Reversible?
 	float orientation = 0.0;
 	float translation_x = 0.0;
 	float translation_y = 0.0;
@@ -21,21 +40,21 @@ task main()
 	float power_BR = 0.0;
 	float power_cap = 0.0;
 	bool doNormalizePower = false;
-	HTGYROstartCal(sensor_gyro);
+	//HTGYROstartCal(sensor_gyro);
 	Joystick_WaitForStart();
 
 	Time_ClearTimer(T1);
 	while (true)
 	{
-		orientation += Time_GetTime(T1)*HTGYROreadRot(sensor_gyro);
-		Time_ClearTimer(T1);
+		//orientation += Time_GetTime(T1)*HTGYROreadRot(sensor_gyro);
+		//Time_ClearTimer(T1);
 
 		Joystick_UpdateData();
 		translation_x = Math_Normalize(Math_TrimDeadband(Joystick_Joystick(JOYSTICK_R, AXIS_X), 10), 128, 100);
 		translation_y = Math_Normalize(Math_TrimDeadband(Joystick_Joystick(JOYSTICK_R, AXIS_Y), 10), 128, 100);
 		rotation = Math_Normalize(Math_TrimDeadband(Joystick_Joystick(JOYSTICK_L, AXIS_X), 10), 128, 100);
 
-		Math_RotateVector(translation_x, translation_y, -orientation);
+		//Math_RotateVector(translation_x, translation_y, -orientation);
 
 		power_FR = translation_y-translation_x-rotation;
 		power_FL = translation_y+translation_x+rotation;
@@ -66,9 +85,18 @@ task main()
 			doNormalizePower = false;
 		}
 
+		if (Joystick_ButtonReleased(BUTTON_A)==true) {
+			isSweeping = (!isSweeping);
+		}
+
 		Motor_SetPower(power_FR, motor_FR);
 		Motor_SetPower(power_FL, motor_FL);
 		Motor_SetPower(power_BL, motor_BL);
 		Motor_SetPower(power_BR, motor_BR);
+		if (isSweeping==true) {
+			Motor_SetPower(100, motor_sweeper);
+		} else {
+			Motor_SetPower(0, motor_sweeper);
+		}
 	}
 }

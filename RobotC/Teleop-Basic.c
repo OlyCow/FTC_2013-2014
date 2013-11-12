5d6be7ae1e12781c7e69061561d6896032c8c586
Ernest Gu <erniewarrensuper@gmail.com>
2013-11-11 17:38:53 -0800

    Renamed and moved vars for clarity.

--- RobotC/Teleop-Basic.c
+++ RobotC/Teleop-Basic.c
@@ -69,10 +69,6 @@ int f_angle_x = 0;
 int f_pos_z = 0;
 int f_pos_y = 0;
 int f_pos_x = 0;
-float power_FR = 0.0;
-float power_FL = 0.0;
-float power_BL = 0.0;
-float power_BR = 0.0;
 float power_sweeper = 0.0;
 float power_lift = 0.0;
 float power_flag = 0.0;
@@ -80,7 +76,11 @@ int lift_target = 0;
 int servo_funnel_L_pos = servo_funnel_L_open;
 int servo_funnel_R_pos = servo_funnel_R_open;
 
-float current_encoder[POD_NUM] = {0,0,0,0};
+float term_P[POD_NUM] = {0,0,0,0};
+float term_I[POD_NUM] = {0,0,0,0};
+float term_D[POD_NUM] = {0,0,0,0};
+
+float pod_current[POD_NUM] = {0,0,0,0};
 
 task main() {
 	initializeGlobalVariables(); // Defined in "initialize.h", this intializes all struct members.
@@ -121,7 +121,7 @@ task main() {
 			if (combined[i].r>g_FullPower) {
 				shouldNormalize = true;
 			}
-			if ((combined[i].theta==0)&&(combined[i].r==0)&&(current_encoder[i]<720)&&(current_encoder>-720)==true) { // AND encoder is within 2 turns.
+			if ((combined[i].theta==0)&&(combined[i].r==0)&&(pod_current[i]<720)&&(pod_current>-720)==true) { // AND encoder is within 2 turns.
 				combined[i].theta = combined_angle_prev[i];
 				// No need to update `combined_angle_prev[i]` because it stays the same.
 				Vector2D_UpdatePos(combined[i]); // This might be unnecessary.
@@ -271,17 +271,17 @@ task PID() {
 		ALIGNED_CLOSE	= 2,
 	};
 
-	float time_current = Time_GetTime(TIMER_PROGRAM);
-	float time_previous = time_current;
-	float time_difference = time_current-time_previous;
+	float t_current = Time_GetTime(TIMER_PROGRAM);
+	float t_prev = t_current;
+	float t_delta = t_current-t_prev;
 	const int kI_delay = 10;
-	float error_accumulated[POD_NUM][kI_delay];
+	float error_sum[POD_NUM][kI_delay];
 	for (int i=0; i<(int)POD_NUM; i++) {
 		for (int j=0; j<kI_delay; j++) {
-			error_accumulated[i][j] = 0;
+			error_sum[i][j] = 0;
 		}
 	}
-	float error_accumulated_total[POD_NUM] = {0,0,0,0}; // {FR, FL, BL, BR}
+	float error_sum_total[POD_NUM] = {0,0,0,0}; // {FR, FL, BL, BR}
 	float kP[POD_NUM] = {0.6,	0.6,	0.6,	0.6}; // TODO: PID tuning.
 	float kI[POD_NUM] = {0.0,	0.0,	0.0,	0.0};
 	float kD[POD_NUM] = {0.0,	0.0,	0.0,	0.0};
@@ -289,13 +289,10 @@ task PID() {
 	float error[POD_NUM] = {0,0,0,0}; // Difference between set-point and measured value.
 	float error_prev[POD_NUM] = {0,0,0,0}; // Easier than using the `error_accumulated` array, and prevents the case where that array is size <=1.
 	float error_rate[POD_NUM] = {0,0,0,0};
-	float term_P[POD_NUM] = {0,0,0,0};
-	float term_I[POD_NUM] = {0,0,0,0};
-	float term_D[POD_NUM] = {0,0,0,0};
-	float total_correction[POD_NUM] = {0,0,0,0}; // Equals "term_P + term_I + term_D".
+	float correction[POD_NUM] = {0,0,0,0}; // Equals "term_P + term_I + term_D".
 	Aligned isAligned = ALIGNED_CLOSE; // If false, cut motor power so that wheel pod can get aligned.
 
-	float lift_pos = 0.0; // Really should be an int.
+	float lift_pos = 0.0; // Really should be an int; using a float so I don't have to cast all the time.
 	float kP_lift = 1.0; // TODO: PID tuning.
 	float kD_lift = 0.0;
 	float error_lift = 0.0;
@@ -308,18 +305,18 @@ task PID() {
 
 	while (true) {
 		// Update timer first, in case something happens later during the loop.
-		time_previous = time_current;
-		time_current = Time_GetTime(TIMER_PROGRAM);
-		time_difference = time_current-time_previous;
+		t_prev = t_current;
+		t_current = Time_GetTime(TIMER_PROGRAM);
+		t_delta = t_current-t_prev;
 
 		for (int i=POD_FR; i<(int)POD_NUM; i++) {
-			current_encoder[i] = Motor_GetEncoder(Motor_Convert((Motor)i))/(float)(-2); // Encoders are geared up by 2 (and "backwards").
-			current_encoder[i] = Math_Normalize(current_encoder[i], (float)1440, 360);
-			current_encoder[i] = (float)(round(current_encoder[i])%360); // Value is now between -360 ~ 360.
-			current_encoder[i] += 360; // Value is now >= 0.
-			current_encoder[i] = (float)(round(current_encoder[i])%360); // Value is now between 0 ~ 360.
+			pod_current[i] = Motor_GetEncoder(Motor_Convert((Motor)i))/(float)(-2); // Encoders are geared up by 2 (and "backwards").
+			pod_current[i] = Math_Normalize(pod_current[i], (float)1440, 360);
+			pod_current[i] = (float)(round(pod_current[i])%360); // Value is now between -360 ~ 360.
+			pod_current[i] += 360; // Value is now >= 0.
+			pod_current[i] = (float)(round(pod_current[i])%360); // Value is now between 0 ~ 360.
 			error_prev[i] = error[i];
-			error[i] = g_ServoData[i].angle-current_encoder[i];
+			error[i] = g_ServoData[i].angle-pod_current[i];
 			if (error[i]>180) {
 				error[i] = error[i]-360;
 			} else if (error[i]<-180) {
@@ -336,13 +333,13 @@ task PID() {
 			//	g_MotorData[i].isReversed = false;
 			//} // TODO: Can the above chain be simplified to something having to do with modulo 90?
 			Math_TrimDeadband(error[i], g_EncoderDeadband);
-			error_accumulated_total[i] -= error_accumulated[i][kI_delay-1]; // Array indices.
-			error_accumulated_total[i] += error_accumulated[i][0];
+			error_sum_total[i] -= error_sum[i][kI_delay-1]; // Array indices.
+			error_sum_total[i] += error_sum[i][0];
 			for (int j=kI_delay-1; j>0; j--) { //`j=kI_delay-1` because we are dealing with array indices.
-				error_accumulated[i][j] = error_accumulated[i][j-1];
+				error_sum[i][j] = error_sum[i][j-1];
 			}
-			error_accumulated[i][0] = error[i]*time_difference;
-			error_rate[i] = (error[i]-error_prev[i])/time_difference;
+			error_sum[i][0] = error[i]*t_delta;
+			error_rate[i] = (error[i]-error_prev[i])/t_delta;
 			if (abs(error[i])>36) { //36 is an arbitrary number :P
 				isAligned = ALIGNED_FAR;
 			} else if (abs(error[i])>12) {
@@ -351,9 +348,9 @@ task PID() {
 				isAligned = ALIGNED_CLOSE;
 			}
 			term_P[i] = kP[i]*error[i];
-			term_I[i] = kI[i]*error_accumulated_total[i];
+			term_I[i] = kI[i]*error_sum_total[i];
 			term_D[i] = kD[i]*error_rate[i];
-			total_correction[i] = Math_Limit((term_P[i]+term_I[i]+term_D[i]), 128);
+			correction[i] = Math_Limit((term_P[i]+term_I[i]+term_D[i]), 128);
 		}
 		for (int i=MOTOR_FR; i<=(int)MOTOR_BR; i++) {
 			switch (isAligned) {
@@ -363,7 +360,7 @@ task PID() {
 				case ALIGNED_MEDIUM:
 					g_MotorData[i].fineTuneFactor *= 1/abs(error[i])*10; // Ranges from 28~83%.
 					break;
-				// Skipping the "ALIGNED_CLOSE" condition might increase performance.
+				// Skipping the "ALIGNED_CLOSE" condition could increase performance.
 			}
 		}
 	}
@@ -382,16 +379,16 @@ task PID() {
 		}
 
 		// Assign the power settings to the servos.
-		Servo_SetPower(servo_FR, -total_correction[POD_FR]);
-		Servo_SetPower(servo_FL, -total_correction[POD_FL]);
-		Servo_SetPower(servo_BL, -total_correction[POD_BL]);
-		Servo_SetPower(servo_BR, -total_correction[POD_BR]);
+		Servo_SetPower(servo_FR, -correction[POD_FR]);
+		Servo_SetPower(servo_FL, -correction[POD_FL]);
+		Servo_SetPower(servo_BL, -correction[POD_BL]);
+		Servo_SetPower(servo_BR, -correction[POD_BR]);
 
 		// Another PID loop, this time for the lift.
 		lift_pos = Motor_GetEncoder(motor_lift);
 		error_prev_lift = error_lift;
 		error_lift = lift_target-lift_pos;
-		error_rate_lift = (error_lift-error_prev_lift)/time_difference;
+		error_rate_lift = (error_lift-error_prev_lift)/t_delta;
 		term_P_lift = kP_lift*error_lift;
 		term_D_lift = kD_lift*error_rate_lift;
 		power_lift = term_P_lift+term_D_lift;
@@ -410,13 +407,13 @@ task CommLink() {
 
 
 
-// Task for displaying stuff on the LCD screen.
+// Task for displaying data on the NXT's LCD screen.
 task Display() {
 
 	typedef enum DisplayMode {
 		DISP_FCS,				// Default FCS screen.
 		DISP_SWERVE_DEBUG,		// Encoders, target values, PID output, power levels.
-		DISP_PID_DEBUG,			// Error, P-term, I-term, D-term.
+		DISP_PID_TUNING,		// Error, P-term, I-term, D-term.
 		DISP_COMM_STATUS,		// Each line of each frame.
 		DISP_ENCODERS,			// Raw encoder values (7? 8?).
 		DISP_SENSORS,			// Might need to split this into two screens.
@@ -451,7 +448,7 @@ task Display() {
 						break;
 					}
 					if (Buttons_Released(NXT_BUTTON_R)==true) {
-						isMode = DISP_PID_DEBUG;
+						isMode = DISP_PID_TUNING;
 						break;
 					}
 					//// TODO: FOLLOWING CODE BLOCK NOT IMPLEMENTED ANYWHERE (COMPILE ERRORS)
@@ -466,7 +463,7 @@ task Display() {
 					//nxtDisplayTextLine(7, "BR encdr%d pow%d", current_encoder[POD_BR], g_MotorData[POD_BR].power);
 				}
 				break;
-			case DISP_PID_DEBUG :
+			case DISP_PID_TUNING :
 				while (true) {
 					bDisplayDiagnostics = false;
 					if (Buttons_Released(NXT_BUTTON_L)==true) {
@@ -483,7 +480,7 @@ task Display() {
 				while (true) {
 					bDisplayDiagnostics = false;
 					if (Buttons_Released(NXT_BUTTON_L)==true) {
-						isMode = DISP_PID_DEBUG;
+						isMode = DISP_PID_TUNING;
 						break;
 					}
 					if (Buttons_Released(NXT_BUTTON_R)==true) {

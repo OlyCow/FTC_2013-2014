81e031a2142831d7db41f51ad8d303b34f1f9e1f
Ernest Gu <erniewarrensuper@gmail.com>
2013-11-16 20:52:49 -0800

    Updated utils to actually work
    Untested, but more theoretically correct.

--- RobotC/Util-SensorTest.c
+++ RobotC/Util-SensorTest.c
@@ -7,8 +7,6 @@
 
 task main()
 {
-	const float loopDelay = 10.0; // milliseconds.
-
 	bDisplayDiagnostics = false;
 	float rotation = 0.0;
 	float heading = 0.0;
@@ -17,23 +15,23 @@ task main()
 	int IR_dirC = 0.0;
 	int IR_dirD = 0.0;
 	int IR_dirE = 0.0;
-	HTGYROstartCal(sensor_gyro);
-	Time_Wait(50);
+	HTGYROstartCal(sensor_gyro); // Does this function actually work? ...
+	Time_Wait(50); // <-unnecessary?
 	HTIRS2setDSPMode(sensor_IR, DSP_1200);
 	Display_Clear();
 	Time_ClearTimer(T1);
 
 	while (true)
 	{
-		rotation = HTGYROreadRot(sensor_gyro);
-		heading += rotation*loopDelay/1000.0; // 1000 seconds per millisecond.
-		nxtDisplayTextLine(1, "Rot: %f", heading*4);
+		heading += ((float)Time_GetTime(T1))*((float)HTGYROreadRot(sensor_gyro))/((float)1000.0); // 1000 milliseconds per second.
+		Time_ClearTimer(T1);
+		nxtDisplayTextLine(1, "Rot: %f", heading);
 		HTIRS2readAllACStrength(sensor_IR, IR_dirA, IR_dirB, IR_dirC, IR_dirD, IR_dirE);
 		nxtDisplayTextLine(3, "IR-A: %d", IR_dirA);
 		nxtDisplayTextLine(4, "IR-B: %d", IR_dirB);
 		nxtDisplayTextLine(5, "IR-C: %d", IR_dirC);
 		nxtDisplayTextLine(6, "IR-D: %d", IR_dirD);
 		nxtDisplayTextLine(7, "IR-E: %d", IR_dirE);
-		Time_Wait(loopDelay);
+		Time_Wait(10);
 	}
 }

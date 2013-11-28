a1c00ea34f6db63a0232ec1ba3307220f760b37c
Ernest Gu <erniewarrensuper@gmail.com>
2013-11-28 08:25:29 -0800

    Basically finished pod reset stuff.
    Just need to add error handling.

--- RobotC/Util-ResetPods.c
+++ RobotC/Util-ResetPods.c
@@ -27,118 +27,115 @@
 #include "Teleop-Basic.h"
 #include "subroutines.h"
 
-
-
 float term_P[POD_NUM]		= {0,0,0,0}; // (P-controller.)
 float rot_raw[POD_NUM]		= {0,0,0,0}; // Encoder value (it's geared down by 2!).
 float rot_error[POD_NUM]	= {0,0,0,0}; // Difference between set-point and measured value.
 
-task Display(); // A separate task for updating the NXT's LCD display.
-
-
-
 task main()
 {
+	Task_Kill(displayDiagnostics);
+	Display_Clear();
+	nxtDisplayTextLine(0, "Initializing vars...");
 	initializeGlobalVariables(); // Defined in "initialize.h", this intializes all struct members.
-	Task_Spawn(Display);
 
-	// Variables for file I/O operations.
+	const int finish_delay = 10*1000; // MAGIC_NUM: milliseconds to delay when program ends.
 	TFileHandle IO_handle;
 	TFileIOResult IO_result;
 	const string file_name = "_reset_data.txt";
 	int file_size = 0;
+	const int resetRange = 10; // MAGIC_NUM: How close the program resets each pod (deg).
+	bool isResetting = true;
+	// kP is always negative because the servos are geared (and reversed).
+	float kP[POD_NUM] = {-1.0, -1.0, -1.0, -1.0}; // MAGIC_NUM: TODO: PID tuning.
 
-	// Variables for PID calculations.
-	float kP[POD_NUM] = {1.0,	1.0,	1.0,	1.0}; // MAGIC_NUM: TODO: PID tuning.
-
+	// Read in all the values of the pods from a text file.
+	nxtDisplayTextLine(1, "Recovering data...");
 	OpenRead(IO_handle, IO_result, file_name, file_size);
 	for (int i=POD_FR; i<(int)POD_NUM; i++) {
-		ReadShort(IO_handle, IO_result, g_ServoData[i].angle); // TODO: Assign values here.
+		ReadShort(IO_handle, IO_result, g_ServoData[i].angle);
+		g_ServoData[i].angle = -g_ServoData[i].angle; // We're going "backwards" to 0 (centering principle!).
 	}
 	Close(IO_handle, IO_result);
 
 	// Press orange button to start resetting.
+	nxtDisplayCenteredTextLine(2, "Press [ENTER] to");
+	nxtDisplayCenteredTextLine(3, "continue.");
+	while (Buttons_Released(NXT_BUTTON_YES)==true) {
+		Buttons_UpdateData();
+		Time_Wait(100); // MAGIC_NUM: Arbitrary LCD refresh delay.
+	}
 
-	while (true) {
+	// Start pod reset (basic P-controller).
+	nxtDisplayTextLine(4, "Resetting pods...");
+	nxtDisplayCenteredTextLine(6, "|     |");
+
+	while (isResetting==true) {
+
+		// If this isn't zero'd here, the loop will never break,
+		// since `isResetting` is calculated with AND statements.
+		isResetting = true;
+
 		// Calculate the targets and error for each wheel pod.
 		for (int i=POD_FR; i<(int)POD_NUM; i++) {
 			rot_raw[i] = Motor_GetEncoder(Motor_Convert((Motor)i))/(float)(-2); // Encoders are geared up by 2 (and "backwards").
 			rot_raw[i] = Math_Normalize(rot_raw[i], (float)1440, 360); // Encoders are 1440 CPR.
 			rot_error[i] = g_ServoData[i].angle-rot_raw[i];
 			term_P[i] = Math_Limit(kP[i]*rot_error[i], 128); // Because servos, not motors.
+			isResetting = isResetting&&(abs(rot_error[i])<resetRange);
 		}
 
 		// Assign the power settings to the servos.
-		Servo_SetPower(servo_FR, -term_P[POD_FR]);
-		Servo_SetPower(servo_FL, -term_P[POD_FL]);
-		Servo_SetPower(servo_BL, -term_P[POD_BL]);
-		Servo_SetPower(servo_BR, -term_P[POD_BR]);
+		Servo_SetPower(servo_FR, term_P[POD_FR]);
+		Servo_SetPower(servo_FL, term_P[POD_FL]);
+		Servo_SetPower(servo_BL, term_P[POD_BL]);
+		Servo_SetPower(servo_BR, term_P[POD_BR]);
+
+		// Update display.
+		nxtDisplayCenteredTextLine(5, "%5d-%5d", -rot_error[POD_FL], -rot_error[POD_FR]);
+		nxtDisplayCenteredTextLine(7, "%5d-%5d", -rot_error[POD_BL], -rot_error[POD_BR]);
 	}
 
 	// Press orange button to confirm jig has been applied.
-
-	// Delete old pod position file.
-
-	// Write new pod position file. (All 0's).
-}
-
-
-
-// Task for displaying data on the NXT's LCD screen.
-// TODO: Put a lot of the display stuff into loops. Do we want to?
-task Display()
-{
-	typedef enum DisplayMode {
-		DISP_FCS,				// Default FCS screen.
-		DISP_PID,
-		DISP_NUM,
-	};
-
-	DisplayMode isMode = DISP_FCS;
-
-	Joystick_WaitForStart();
-
-	while (true) {
+	Display_Clear();
+	nxtDisplayCenteredTextLine(0, "continue.");
+	nxtDisplayTextLine(1, "Resetting pods...");
+	nxtDisplayCenteredTextLine(2, "%5d-%5d", -rot_error[POD_FL], -rot_error[POD_FR]);
+	nxtDisplayCenteredTextLine(3, "|     |");
+	nxtDisplayCenteredTextLine(4, "%5d-%5d", -rot_error[POD_BL], -rot_error[POD_BR]);
+	nxtDisplayCenteredTextLine(5, "Apply jig, then");
+	nxtDisplayCenteredTextLine(6, "press [ENTER] to");
+	nxtDisplayCenteredTextLine(7, "continue.");
+	while (Buttons_Released(NXT_BUTTON_YES)==true) {
 		Buttons_UpdateData();
-
-		switch (isMode) {
-			case DISP_FCS :
-				break;
-			case DISP_PID :
-				// The value of `pod_current[i]` is (should be?) between 0~360.
-				nxtDisplayTextLine(0, "FR encdr%3d P%3d", pod_current[POD_FR], g_ServoData[POD_FR].angle);
-				nxtDisplayTextLine(1, "FL encdr%3d P%3d", pod_current[POD_FL], g_ServoData[POD_FL].angle);
-				nxtDisplayTextLine(2, "BL encdr%3d P%3d", pod_current[POD_BL], g_ServoData[POD_BL].angle);
-				nxtDisplayTextLine(3, "BR encdr%3d P%3d", pod_current[POD_BR], g_ServoData[POD_BR].angle);
-				nxtDisplayCenteredTextLine(4, "err%+4d", correction_pod[POD_FR], g_MotorData[POD_FR].power);
-				nxtDisplayCenteredTextLine(5, "err%+4d", correction_pod[POD_FL], g_MotorData[POD_FL].power);
-				nxtDisplayCenteredTextLine(6, "err%+4d", correction_pod[POD_BL], g_MotorData[POD_BL].power);
-				nxtDisplayCenteredTextLine(7, "err%+4d", correction_pod[POD_BR], g_MotorData[POD_BR].power);
-				break;
-			default :
-				nxtDisplayCenteredTextLine(3, "This debug screen");
-				nxtDisplayCenteredTextLine(4, "does not exist.");
-				break;
-		}
-
-		if (Buttons_Released(NXT_BUTTON_L)==true) {
-			Display_Clear();
-			isMode = (DisplayMode)((isMode+DISP_NUM-1)%DISP_NUM);
-			if (isMode==DISP_FCS) {
-				bDisplayDiagnostics = true;
-			} else {
-				bDisplayDiagnostics = false;
-			}
-		}
-		if (Buttons_Released(NXT_BUTTON_R)==true) {
-			Display_Clear();
-			isMode = (DisplayMode)((isMode+DISP_NUM+1)%DISP_NUM);
-			if (isMode==DISP_FCS) {
-				bDisplayDiagnostics = true;
-			} else {
-				bDisplayDiagnostics = false;
-			}
-		}
-		Time_Wait(100); // MAGIC_NUM: Prevents the LCD from updating itself to death. (Okay, maybe not that dramatic.)
+		Time_Wait(100); // MAGIC_NUM: Arbitrary LCD refresh delay.
 	}
+
+	// Delete old pod position file, create a new one, and write 0's to it.
+	Display_Clear();
+	nxtDisplayTextLine(0, "Resetting pods...");
+	nxtDisplayCenteredTextLine(1, "%5d-%5d", -rot_error[POD_FL], -rot_error[POD_FR]);
+	nxtDisplayCenteredTextLine(2, "|     |");
+	nxtDisplayCenteredTextLine(3, "%5d-%5d", -rot_error[POD_BL], -rot_error[POD_BR]);
+	nxtDisplayCenteredTextLine(4, "Apply jig, then");
+	nxtDisplayCenteredTextLine(5, "press [ENTER] to");
+	nxtDisplayCenteredTextLine(6, "continue.");
+	nxtDisplayTextLine(7, "Rewriting position...");
+	Delete(file_name, IO_result);
+	OpenWrite(IO_handle, IO_result, file_name, file_size);
+	for (int i=POD_FR; i<(int)POD_NUM; i++) {
+		WriteShort(IO_handle, IO_result, 0);
+	}
+	Close(IO_handle, IO_result);
+
+	// Alert user that program has terminated, and delay before ending.
+	Display_Clear();
+	nxtDisplayCenteredTextLine(0, "%5d-%5d", -rot_error[POD_FL], -rot_error[POD_FR]);
+	nxtDisplayCenteredTextLine(1, "|     |");
+	nxtDisplayCenteredTextLine(2, "%5d-%5d", -rot_error[POD_BL], -rot_error[POD_BR]);
+	nxtDisplayCenteredTextLine(3, "Apply jig, then");
+	nxtDisplayCenteredTextLine(4, "press [ENTER] to");
+	nxtDisplayCenteredTextLine(5, "continue.");
+	nxtDisplayTextLine(6, "Rewriting position...");
+	nxtDisplayTextLine(7, "Finished.");
+	Time_Wait(finish_delay);
 }

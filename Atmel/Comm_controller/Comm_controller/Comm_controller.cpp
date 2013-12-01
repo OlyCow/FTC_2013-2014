3d73f808aedb8a19ee8896a228859a59b1735306
Ernest Gu <erniewarrensuper@gmail.com>
2013-11-30 19:07:48 -0800

    Set up a template for the comm AVR.

--- Atmel/Comm_controller/Comm_controller/Comm_controller.cpp
+++ Atmel/Comm_controller/Comm_controller/Comm_controller.cpp
@@ -1,18 +1,27 @@
 // For communicating with the NXT (via the SuperPro board).
 
+#include <avr/io.h>
+#include <avr/interrupt.h>	// Include <util/atomic.h> for proper use.
+#include <util/atomic.h>	// For interrupts^.
+#include <util/twi.h>
+
 #ifndef F_CPU
 #define F_CPU 1000000UL
 #endif
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
-#include <util/twi.h>
+
 #include <math.h>
+
+#include "Comm_controller.h"
 
 int main(void)
 {
+	setupPins();
 	while (true) {
-		// TODO:: Write actual code.
+		// TODO: Write actual code.
+		// Initialize.
+		// Read data from other AVRs.
+		// Read data from NXT.
+		// Write data to NXT.
 	}
 }
---
+++ Atmel/Comm_controller/Comm_controller/Comm_controller.h
@@ -0,0 +1,7 @@
+// For communicating with the NXT (via the SuperPro board).
+
+void setupPins();
+
+void setupPins() {
+	// Setup pins here. Ports can be redefined as well.
+}
